9b3f419c48d1ba14e28494d9de1059fd77ea5da6
kishore951 <kishoresakthivel951@gmail.com>
2022-10-13 16:04:43 +0530

    again

--- src/app/add-donar/add-donar.component.ts
+++ src/app/add-donar/add-donar.component.ts
@@ -9,6 +9,40 @@ export class AddDonarComponent implements OnInit {
 
   constructor() { }
 
+  name=""
+  bloodGroup=""
+  dob=""
+  lastDonationDate=""
+  previousDisease=""
+  lastVaccinationDate=""
+  address=""
+  pinCode=""
+  mobileNo=""
+  emailId=""
+
+
+  
+
+  readValues=()=>{
+    let data={
+  "name":this.name,
+  "bloodGroup":this.bloodGroup,
+  "dob":this.dob,
+  "lastDonationDate":this.lastDonationDate,
+  "previousDisease":this.previousDisease,
+  "lastVaccinationDate":this.lastVaccinationDate,
+  "address":this.address,
+  "pinCode":this.pinCode,
+  "mobileNo":this.mobileNo,
+  "emailId":this.emailId
+
+    }
+    console.log(data)
+  }
+
+
+  
+
   ngOnInit(): void {
   }
 
